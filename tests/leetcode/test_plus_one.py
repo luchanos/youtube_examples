918ec418fd5480994b9b68180b8a138f432082e0
luchanos <lukash_91@mail.ru>
2022-02-23 14:02:39 +0300

    [upd] tasks

---
+++ tests/leetcode/test_plus_one.py
@@ -0,0 +1,23 @@
+"""
+https://leetcode.com/problems/plus-one/
+"""
+
+import pytest
+from typing import List
+
+
+class Solution:
+    def plusOne(self, digits: List[int]) -> List[int]:
+        num = 0
+        for cnt in range(len(digits)):
+            num += digits[cnt] * 10 ** (len(digits) - cnt - 1)
+        num += 1
+        return [int(x) for x in str(num)]
+
+
+@pytest.mark.parametrize("input_value, expected", [([1, 2, 3], [1, 2, 4]),
+                                                   ([4, 3, 2, 1], [4, 3, 2, 2]),
+                                                   ([9], [1, 0])])
+def test_palidrome(input_value, expected):
+    sol = Solution()
+    assert sol.plusOne(input_value) == expected
