8bf80c205f7275705e46ef323d0f57191af61125
luchanos <lukash_91@mail.ru>
2022-05-06 18:47:27 +0400

    [upd] better

--- netology/networks/py_wi_fi.py
+++ netology/networks/py_wi_fi.py
@@ -4,3 +4,10 @@ import time
 wifi = pywifi.PyWiFi()
 iface = wifi.interfaces()[0]
 print("Мой интерфейс", iface.name)
+
+iface.scan()
+time.sleep(2)
+result = iface.scan_results()
+print("\nДоступные сети:")
+for i in range(len(result)):
+    print(result[i].ssid)
