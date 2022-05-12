9f174a5aa0260bd97fba883623f3fb11de953d01
luchanos <lukash_91@mail.ru>
2022-05-13 00:27:56 +0400

    [upd] better

--- materials/concurrency/procceses/procces_queue.py
+++ materials/concurrency/procceses/procces_queue.py
@@ -1,19 +1,24 @@
-from multiprocessing import Process
+from multiprocessing import Process, Queue
 from time import sleep
 
 
-def func(a):
+def func(a, q):
     cnt = 0
     while True:
         sleep(1)
         print(a)
         cnt += 1
+        q.put(a)
         if cnt > 2:
             break
 
 
 if __name__ == "__main__":
-    p = Process(target=func, args=(1, ))
+    q = Queue(maxsize=100)
+    p = Process(target=func, args=(1, q))
     p.start()
-    p1 = Process(target=func, args=(2, ))
+    p1 = Process(target=func, args=(2, q))
     p1.start()
+    p1.join()
+    p.join()
+    print("result", q.get())
