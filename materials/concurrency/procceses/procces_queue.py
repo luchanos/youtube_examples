042f4ca7d35473820cc46b1ee0ec37614233e640
luchanos <lukash_91@mail.ru>
2022-05-16 03:36:56 +0400

    [upd] better

--- materials/concurrency/procceses/procces_queue.py
+++ materials/concurrency/procceses/procces_queue.py
@@ -1,14 +1,4 @@
-from multiprocessing import Queue, Pipe, Process
-from time import sleep
-
-
-def worker(a: int, q: Queue):
-    cnt = 0
-    while cnt < 3:
-        sleep(1)  # какое-то долгое вычисление
-        print(a)
-        cnt += 1
-        q.put(a)
+from multiprocessing import Pipe, Process
 
 
 def pipe_worker(p: Pipe):
@@ -17,34 +7,15 @@ def pipe_worker(p: Pipe):
 
 
 def pipe_worker_2(p: Pipe):
-    print(p.recv())
-
-
-def f(a):
-    return a + a
+    print("!!!", p.send(111))
 
 
 if __name__ == "__main__":
-    # работаем с очередями
-    # q = Queue(maxsize=100)
-    # p = Process(target=worker, args=(1, q))
-    # p.start()
-    # p1 = Process(target=worker, args=(2, q))
-    # p1.start()
-    # p1.join()
-    # p.join()
-    # print("24 result", q.get())
-    # print("25 result", q.get())
-    # print("26 result", q.get())
-    # print("27 result", q.get())
-    # print("28 result", q.get())
-    # print("29 result", q.get())
-
     # работаем с конвейерами
-    parent_pipe, child_pipe = Pipe(duplex=True)
+    parent_pipe, child_pipe = Pipe()
     p = Process(target=pipe_worker, args=(child_pipe, ))
     p.start()
-    p1 = Process(target=pipe_worker_2, args=(child_pipe, ))
+    p1 = Process(target=pipe_worker_2, args=(child_pipe,))
     p1.start()
     print("Информация из дочернего процесса:", parent_pipe.recv())
     parent_pipe.send("Информация из главного процесса")
