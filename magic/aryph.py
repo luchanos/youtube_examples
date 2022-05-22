405b5fb39ae227d5d377d52c2af35f12be233ed7
luchanos <lukash_91@mail.ru>
2022-05-22 18:50:43 +0400

    [upd] better

--- magic/aryph.py
+++ magic/aryph.py
@@ -7,28 +7,30 @@ class Maxtrix:
         for i in range(len(self.rows[0])):
             new_string = []
             for j in range(len(self.rows[0])):
-                new_string.append(self.rows[i][j] + other[i][j])
+                new_string.append(self.rows[i][j] + other.rows[i][j])
             res_matr.append(new_string)
         return Maxtrix(res_matr)
 
     def __str__(self):
         s = ""
         for line in self.rows:
-            s += " ".join(line) + "\n"
+            s += " ".join(list(map(str, line))) + "\n"
         return s
 
 
-m_1 = [
+m_1 = Maxtrix([
     [1, 1, 1],
     [1, 1, 1],
     [1, 1, 1]
-]
+])
 
-m_2 = [
+m_2 = Maxtrix([
     [1, 1, 1],
     [1, 1, 1],
     [1, 1, 1]
-]
+])
+
+print(m_1)
 
 c = 1
 m_3 = m_1 + m_2
